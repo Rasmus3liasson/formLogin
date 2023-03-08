9adbadeeee975c5f15aff98bb8621c10c014891a
Rasmus Eliasson <ras.muse@hotmail.com>
2023-03-08 20:35:07 +0100

    added icon to show password

--- src/index.js
+++ src/index.js
@@ -8,6 +8,23 @@ const password = document.querySelector("#password");
 const passwordConfirm = document.querySelector("#confirm-password");
 const telefon = document.querySelector("#telefon");
 const btnTag = document.querySelector("#btn-tag");
+
+const eyeIcon = document
+  .querySelector("#eye-icon")
+  .addEventListener("click", function () {
+    if (password.getAttribute("type", "password")) {
+      password.setAttribute("type", "text");
+    }
+  });
+
+const eyeIconConfirm = document
+  .querySelector("#eye-icon-confirm")
+  .addEventListener("click", function () {
+    if (passwordConfirm.getAttribute("type", "password")) {
+      passwordConfirm.setAttribute("type", "text");
+    }
+  });
+
 let subBtn = document.querySelector("#submit-btn");
 
 function checkPassword() {
@@ -16,7 +33,6 @@ function checkPassword() {
     password.value.length >= 8 &&
     password.value === passwordConfirm.value
   ) {
-    console.log(password.value);
     return true;
   } else {
     password.classList.add("input-error");
@@ -29,7 +45,6 @@ function checkEmail() {
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
   if (mail.value.match(emailPattern) && mail.value === mailConfirm.value) {
-    console.log(mail.value);
     return true;
   } else {
     mail.classList.add("input-error");
@@ -37,54 +52,32 @@ function checkEmail() {
   }
 }
 
-function showBorder() {
-  if (userName.value === "") {
-    userName.classList.add("input-error");
-  } else if (userName.value != "") {
-    userName.classList.remove("input-error");
-  }
-  if (firstName.value === "") {
-    firstName.classList.add("input-error");
-  } else if (firstName.value != "") {
-    firstName.classList.remove("input-error");
-  }
-  if (lastName.value === "") {
-    lastName.classList.add("input-error");
-  } else if (lastName.value != "") {
-    lastName.classList.remove("input-error");
-  }
-  if (mail.value === "") {
-    mail.classList.add("input-error");
-  } else if (mail.value != "") {
-    mail.classList.remove("input-error");
-  }
-  if (mailConfirm.value === "") {
-    mailConfirm.classList.add("input-error");
-  } else if (mailConfirm.value != "") {
-    mailConfirm.classList.remove("input-error");
-  }
-  if (password.value === "") {
-    password.classList.add("input-error");
-  } else if (password.value != "") {
-    password.classList.remove("input-error");
-  }
-  if (passwordConfirm.value === "") {
-    passwordConfirm.classList.add("input-error");
-  } else if (passwordConfirm.value != "") {
-    passwordConfirm.classList.remove("input-error");
-  }
-  if (telefon.value === "") {
-    telefon.classList.add("input-error");
-  } else if (telefon.value != "") {
-    telefon.classList.remove("input-error");
+function errorBorder(inputValue) {
+  if (inputValue.value === "") {
+    inputValue.classList.add("input-error");
+  } else if (inputValue.value != "") {
+    inputValue.classList.remove("input-error");
   }
 }
 
+function errorIndicator() {
+  errorBorder(userName);
+  errorBorder(firstName);
+  errorBorder(lastName);
+  errorBorder(mail);
+  errorBorder(mailConfirm);
+  errorBorder(password);
+  errorBorder(passwordConfirm);
+  errorBorder(telefon);
+}
+
 subBtn.addEventListener("click", function () {
-  showBorder();
-  event.preventDefault();
-  if (checkEmail() && checkPassword() === true) {
+  errorIndicator();
+
+  if (checkEmail() && checkPassword()) {
     console.log("klarad");
     btnTag.setAttribute("href", "signIn.html");
+  } else {
+    event.preventDefault();
   }
 });
